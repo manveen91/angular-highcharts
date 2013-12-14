2b04b4780ddd02ab804c26ef9295aa7bcd93cb20
Jurgen Van de Moere <jurgen@webberid.com>
2013-12-14 13:53:11 +0100

    Added BaseChartConfig service factory

---
+++ src/showpadHighcharts/services/baseChartConfig.js
@@ -0,0 +1,7 @@
+angular.module('showpadHighcharts.services')
+    .factory('BaseChartConfig', ['showpadHighcharts.config', function(config){
+
+        var BaseChartConfig = function BaseChartConfig(){};
+
+        return BaseChartConfig;
+    }]);
