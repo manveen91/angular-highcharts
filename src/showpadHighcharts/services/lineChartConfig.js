850067b5235a74309b73038bab43e8676a48f510
Jurgen Van de Moere <jurgen@webberid.com>
2013-12-14 14:03:38 +0100

    Added comments

--- src/showpadHighcharts/services/lineChartConfig.js
+++ src/showpadHighcharts/services/lineChartConfig.js
@@ -2,9 +2,12 @@ angular.module('showpadHighcharts.services')
     .factory('LineChartConfig', ['showpadHighcharts.config', 'BaseChartConfig', function (config, BaseChartConfig) {
 
         var LineChartConfig = function LineChartConfig() {
+
+            // Call parent constructor
             BaseChartConfig.call(this, arguments);
         };
 
+        // Initialize prototype as new object who's prototype is the BaseCharConfig prototype
         LineChartConfig.prototype = new (angular.extend(
             function () {
             },
