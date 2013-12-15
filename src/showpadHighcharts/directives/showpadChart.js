c0b1f8f24d70296f958b7cc74000ca9315a85d4e
Jurgen Van de Moere <jurgen@webberid.com>
2013-12-15 12:19:59 +0100

    Added showpadChartController (directive controller) and ngModelController for exposing a custom API to the developer

--- src/showpadHighcharts/directives/showpadChart.js
+++ src/showpadHighcharts/directives/showpadChart.js
@@ -2,20 +2,79 @@ angular.module('showpadHighcharts.directives')
     .directive('showpadChart', ['showpadHighcharts.config', 'BaseChartConfig', function (showpadHighchartsConfig, BaseChartConfig) {
 
         return{
-            restrict: 'A',
-            link    : function (scope, iElement, iAttrs) {
+            restrict  : 'A',
+            require   : ['showpadChart', '?ngModel'],
+            controller: ['$scope', '$element', '$attrs', '$transclude', function ($scope, $element, $attrs, $transclude) {
 
-                // Evaluate config in current scope
+                // Placeholder for element
+                this._element = $element[0];
+
+                // Placeholder for chart API returned by Highcharts
+                // to allow access to the API
+                this._chart = void 0;
+
+                // Placeholder for config for this directive
+                // This allows us to make changes to this config
+                // without affectingt the original config in the scope
+                // this._config = jQuery.extend(true, {}, $scope.$eval($attrs.showpadChart));
+
+                // Define properties we wish to expose
+                Object.defineProperties(this, {
+                    element: {
+                        get: function(){
+                            return this._element;
+                        },
+                        configurable: false
+                    },
+                    chart: {
+                        get: function(){
+                            return this._chart;
+                        },
+                        configurable: false
+                    },
+                    config: {
+                        get: function(){
+                            return this._config;
+                        },
+                        configurable: false
+                    }
+                });
+
+            }],
+            link      : function (scope, iElement, iAttrs, controllers) {
+
+                console.log('link');
+                console.dir(controllers);
+                console.dir(scope);
+
+                // Define controller(s)
+                var showpadChartController = controllers[0];
+                var ngModelController = controllers[1];
+
+                // Get config to work with
+                // We don't use the scope.config to avoid changes to affect the original config in the scope
                 var config = scope.$eval(iAttrs.showpadChart);
 
-                // Make sure config is as expected
+                // Make sure config passed as argument is a valid config
                 if (!(config instanceof BaseChartConfig)) {
                     console.log('Skipped rendering chart because config is not instance of BaseChartConfig');
                     return;
                 }
 
-                // Create chart
-                $(iElement).highcharts(config);
+                // Create chart programmatically so we get a handle of the API
+                var element = iElement[0];
+                config.chart.renderTo = element;
+                var chart = new Highcharts.Chart(config);
+
+                // Assign showpadController chart
+                if(showpadChartController){
+                    showpadChartController._chart = chart;
+                }
+
+                // Set initial model value if a model is defined
+                if (ngModelController) {
+                    ngModelController.$setViewValue(showpadChartController);
+                }
 
                 // Debugging
                 if (showpadHighchartsConfig.debug) {
