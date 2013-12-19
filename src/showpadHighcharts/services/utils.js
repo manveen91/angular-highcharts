9ac86bb45c16a95d874497febe30ffd2994f1ba2
Jurgen Van de Moere <jurgen@webberid.com>
2013-12-19 06:55:56 +0100

    Added Utils service to provide helper functions such as deepExtend

---
+++ src/showpadHighcharts/services/utils.js
@@ -0,0 +1,22 @@
+angular.module('showpadHighcharts.services')
+    .factory('Utils', ['showpadHighcharts.config', function (config) {
+
+        return {
+
+            // Deep extend method as explained here:
+            // http://andrewdupont.net/2009/08/28/deep-extending-objects-in-javascript/
+            deepExtend: function (destination, source) {
+                for (var property in source) {
+                    if (source[property] && source[property].constructor &&
+                        source[property].constructor === Object) {
+                        destination[property] = destination[property] || {};
+                        arguments.callee(destination[property], source[property]);
+                    } else {
+                        destination[property] = source[property];
+                    }
+                }
+                return destination;
+            }
+        };
+
+    }]);
