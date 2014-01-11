5f1012d9b605707d2234e4d7431dd1b1b4861b45
Jurgen Van de Moere <jurgen@webberid.com>
2014-01-11 14:35:35 +0100

    Add example page to test memory consumption

---
+++ examples/memory-consumption/js/app.js
@@ -0,0 +1,64 @@
+angular.module('app', ['showpadHighcharts'])
+
+    .controller('IndexCtrl', ['$scope', 'LineChartConfig', function($scope, LineChartConfig){
+
+        // Help function to create mock data
+        var createMockSeriesData = function(numberOfPoints, max){
+            var data = [];
+            for(var i = 0; i < numberOfPoints; i++){
+                data.push(Math.random() * max);
+            }
+            return data;
+        };
+
+        // Placeholder for Chart API
+        $scope.chart = void 0;
+
+        // Chart config
+        $scope.chartConfig = new LineChartConfig({
+            title: {
+                text: 'Showpad Highcharts demo'
+            },
+            xAxis: {
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+            },
+            series: [{
+                data: createMockSeriesData(12, 10),
+                step: 'right',
+                name: 'Right'
+            }, {
+                data: createMockSeriesData(12, 10),
+                step: 'center',
+                name: 'Center'
+            }, {
+                data: createMockSeriesData(12, 10),
+                step: 'left',
+                name: 'Left'
+            }]
+        });
+
+        // Helper function to update chart data to simulate new data coming in
+        var timer = setInterval(function(){
+
+            // Grab chart from API provided by showpadChart
+            var chart = $scope.chart.chart;
+
+            if(chart){
+                console.log('update series ' + $scope.$id);
+                chart.series[0].setData(createMockSeriesData(12, 10), false);
+                chart.series[1].setData(createMockSeriesData(12, 15), false);
+                chart.series[2].setData(createMockSeriesData(12, 20), false);
+
+                console.log('redraw in scope ' + $scope.$id);
+                chart.redraw();
+            }
+        }, 2000);
+
+        $scope.$on('$destroy', function(){
+            clearInterval(timer);
+            console.log('Removed interval from scope ' + $scope.$id);
+        })
+
+    }])
+
+;
